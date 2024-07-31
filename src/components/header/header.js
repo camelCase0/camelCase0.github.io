05d4216b5b509b24f331d51919f819dbdc3d7523
camelCase0 <pashok2082.ryuz@gmail.com>
2024-08-01 01:44:11 +0300

    margin

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,18 +5,18 @@ import "./header.css";
 export default class Header extends Component {
   render() {
     return (
-      <div className="row m-1">
+      <div className="row m-1 mt-2">
         <a className="col-2 btn btn-primary" aria-current="page" href="#">
           StarDB
         </a>
         <ul className="col-10 nav justify-content-center">
-          <li className="nav-item">
+          <li className="nav-item mr-5">
             <Link to="/people">People</Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item mr-5">
             <Link to="/planet">Planet</Link>
           </li>
-          <li className="nav-item">
+          <li className="nav-item mr-5">
             <Link to="/starship">Starships</Link>
           </li>
         </ul>
