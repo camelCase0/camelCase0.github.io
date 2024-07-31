bca4e67e209d853f9b30224eb569cdd3d51eadc4
camelCase0 <pashok2082.ryuz@gmail.com>
2024-08-01 01:36:17 +0300

    Link to

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,36 +1,23 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./header.css";
 
 export default class Header extends Component {
   render() {
     return (
       <div className="row m-1">
-        <a
-          className="col-2 btn btn-primary"
-          aria-current="page"
-          href="/star-db"
-        >
+        <a className="col-2 btn btn-primary" aria-current="page" href="#">
           StarDB
         </a>
         <ul className="col-10 nav justify-content-center">
           <li className="nav-item">
-            <a
-              className="nav-link active"
-              aria-current="page"
-              href="/star-db/people"
-            >
-              People
-            </a>
+            <Link to="/people">People</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/star-db/planet">
-              Planet
-            </a>
+            <Link to="/planet">Planet</Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="/star-db/starship">
-              Starships
-            </a>
+            <Link to="/starship">Starships</Link>
           </li>
         </ul>
       </div>
